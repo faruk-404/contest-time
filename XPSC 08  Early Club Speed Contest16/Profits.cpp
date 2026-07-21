8711a7213ef66a1693977e9068d559d5009dbd47
MD Faruk Hossain <faruk1572005@gmail.com>
2026-07-22 01:02:16 +0600

    21/01/2026

---
+++ XPSC 08  Early Club Speed Contest16/Profits.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+#define nf cout<<'\n'
+#define int long long
+#define cy cout << "YES\n"
+#define cn cout << "NO\n"
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+
+void solve(){
+    int n,k;cin>>n>>k;
+    if( n<=k){cout<<0<<nl;return;}
+    int ans= ((n*(n+1))/2) -((k*(k-1))/2)-((n-k+1)*k);
+    cout<<ans<<nl;
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--){solve();}
+    return 0;
+}
